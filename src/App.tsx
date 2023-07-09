fc72f54815f27966936c501b501fd68fe9cf5f62
Mukthahar26 <mukthaharshaik@gmail.com>
2023-07-09 09:52:19 +0530

    deleted unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigators from './navigations/rootNavigators';
-import {
-  UrlListContext,
-  urlListStateInit,
-  urlListStateInitProps,
-  urlListReducer,
-} from './contextAPIs/urlsContext';
 
 const App = () => {
-  const [state, dispatch] = useReducer(
-    urlListReducer,
-    urlListStateInit as urlListStateInitProps,
-  );
   return (
-    <UrlListContext.Provider value={{state, dispatch}}>
-      <NavigationContainer>
-        <RootNavigators />
-      </NavigationContainer>
-    </UrlListContext.Provider>
+    <NavigationContainer>
+      <RootNavigators />
+    </NavigationContainer>
   );
 };
 
